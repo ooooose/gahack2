58ffd48cf0ada189f9f9a77f6a767da0c3b219b0
ooooose <yuuki.oose1990@gmail.com>
2023-07-22 08:14:59 +0900

    テーマモデルの作成

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_20_215522) do
+ActiveRecord::Schema.define(version: 2023_07_21_230755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "themes", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "uid", null: false
