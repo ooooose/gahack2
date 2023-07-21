29ec20ac52c782360ac4bcdfc19e82f0bca98127
ooooose <yuuki.oose1990@gmail.com>
2023-07-22 07:32:53 +0900

    タイポ修正

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :user, presence: true, length: { maximum: 40 }
+  validates :name, presence: true, length: { maximum: 40 }
   validates :uid, presence: true, uniqueness: true
   validates :description, length: { maximum: 160 }
 end
