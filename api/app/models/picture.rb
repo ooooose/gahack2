98676d0bf256628ff1e5bf4c68ac63b2825a2f4e
ooooose <yuuki.oose1990@gmail.com>
2023-07-22 14:47:14 +0900

    modelファイルの設定

--- api/app/models/picture.rb
+++ api/app/models/picture.rb
@@ -1,2 +1,17 @@
+# Table name: pictures
+#
+#  id         :bigint           not null, primary key
+#  image      :string           not null
+#  frame_id   :integer          not null, default: 0
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+#  Foreign Keys
+#
+#  theme_id => themes.id
+#  user_id => users.id
+#
 class Picture < ApplicationRecord
+  belongs_to :user
+  belongs_to :theme
 end
--- api/app/models/theme.rb
+++ api/app/models/theme.rb
@@ -1,2 +1,12 @@
+# Table name: themes 
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Theme < ApplicationRecord
+  has_many :pictures, -> { order(created_at: :desc) }, dependent: :destroy
+
+  validates :title, presence: true, uniqueness: true
 end
