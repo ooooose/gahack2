1bf36aaf72cb307c0ad7ce0137049bf584aa6f36
ooooose <yuuki.oose1990@gmail.com>
2023-07-23 21:29:21 +0900

    RSpec追記

--- api/spec/models/picture_spec.rb
+++ api/spec/models/picture_spec.rb
@@ -1,8 +1,25 @@
 require "rails_helper"
 
 RSpec.describe Picture, type: :model do
+  before do
+    @user = create(:user)
+    @theme = create(:theme)
+  end
+
   it "is valid picture" do
     picture = create(:picture)
     expect(picture).to be_valid
+  end
+
+  it "is invalid picture of no image" do
+    picture = build(:picture, image: nil)
+    picture.valid?
+    expect(picture).not_to be_valid
+  end
+
+  it "is invalid picture of no frame_id" do
+    picture = build(:picture, frame_id: nil)
+    picture.valid?
+    expect(picture).not_to be_valid
   end
 end
