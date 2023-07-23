57d33465297fc1d0f54485b45d869538bad326e2
ooooose <yuuki.oose1990@gmail.com>
2023-07-23 11:23:19 +0900

    テーマのRSpecを記述

--- api/spec/factories/themes.rb
+++ api/spec/factories/themes.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
   factory :theme do
+    sequence(:title) { |n| "title_#{n}" }
   end
 end
--- api/spec/models/theme_spec.rb
+++ api/spec/models/theme_spec.rb
@@ -1,5 +1,26 @@
 require "rails_helper"
 
 RSpec.describe Theme, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "valid theme" do
+    theme = create(:theme)
+    expect(theme).to be_valid
+  end
+
+  it "invalid theme of duplicated title" do
+    Theme.create(
+      title: "ドラえもん",
+    )
+
+    theme = Theme.new(
+      title: "ドラえもん",
+    )
+    theme.valid?
+    expect(theme).not_to be_valid
+  end
+
+  it "invalid theme of no title" do
+    theme = build(:theme, title: nil)
+    theme.valid?
+    expect(theme).not_to be_valid
+  end
 end
