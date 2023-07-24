f7cc0b3a65bf29892ccc22d38ef4b540965cb92f
ooooose <yuuki.oose1990@gmail.com>
2023-07-25 07:10:36 +0900

    ダミーデータ追加

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Theme.find_or_create_by(title: "カエル")
+Theme.find_or_create_by(title: "腕を組む人")
+Theme.find_or_create_by(title: "怒ってるモチ")
