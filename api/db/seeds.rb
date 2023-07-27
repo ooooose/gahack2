38e64de3e865e196c2fcea94d878f71ca99d02ba
ooooose <yuuki.oose1990@gmail.com>
2023-07-28 06:47:19 +0900

    [Add]ダミーデータの投入

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,3 +1,9 @@
-Theme.find_or_create_by(title: "カエル")
-Theme.find_or_create_by(title: "腕を組む人")
-Theme.find_or_create_by(title: "怒ってるモチ")
+theme_1 = Theme.find_or_create_by(title: "カエル")
+theme_2 = Theme.find_or_create_by(title: "腕を組む人")
+theme_3 = Theme.find_or_create_by(title: "怒ってるモチ")
+
+user = User.find_or_create_by(uid: 'aaa', name: 'oose', twitter_name: 'oose', avatar: 'sample_avatar', description: '')
+
+Picture.find_or_create_by(user_id: user.id, theme_id: theme_1.id, image: 'image_a', frame_id: 0)
+Picture.find_or_create_by(user_id: user.id, theme_id: theme_2.id, image: 'image_b', frame_id: 0)
+Picture.find_or_create_by(user_id: user.id, theme_id: theme_3.id, image: 'image_c', frame_id: 0)
