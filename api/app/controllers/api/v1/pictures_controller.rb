a90239c8b6f408ec71e10603dcc44842c2513f73
ooooose <yuuki.oose1990@gmail.com>
2023-07-28 06:49:51 +0900

    [Add]アクションの作成

--- api/app/controllers/api/v1/pictures_controller.rb
+++ api/app/controllers/api/v1/pictures_controller.rb
@@ -2,14 +2,18 @@ class Api::V1::PicturesController < BaseController
   before_action :set_picture, only: %i[show]
 
   def index
+    pictures = Picture.includes(:user, :theme).all
+
+    render json: pictures
   end
 
   def show
+    render json: @picture
   end
 
   private
 
     def set_picture
-      @picture = Picture.find(params[:id])
+      @picture = Picture.includes(:user, :theme).find(params[:id])
     end
 end
