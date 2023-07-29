5dd3d6c79990a224ffd1db942f84732ac95079c4
ooooose <yuuki.oose1990@gmail.com>
2023-07-30 05:42:25 +0900

    workflowの変更

--- api/spec/models/like_spec.rb
+++ api/spec/models/like_spec.rb
@@ -1,28 +1,40 @@
 require "rails_helper"
 
 RSpec.describe Like, type: :model do
-  before do
-    @user = create(:user)
-    @picture = create(:picture)
-    @like = create(:like, user_id: @user.id, picture_id: @picture.id)
+  describe "create like model" do
+    before do
+      @user = create(:user)
+      @picture = create(:picture)
+    end
+    let!(:like) { create(:like, user_id: @user.id, picture_id: @picture.id) }
+    let(:other_like) { build(:like, user_id: @user.id, picture_id: @picture.id) }
+
+    context "when is valid like" do
+      it "is valid like instance" do
+        expect(like).to be_valid
+      end
+    end
+
+    context "when is invalid like" do
+      it "is invalid" do
+        other_like.valid?
+
+        expect(other_like).not_to be_valid
+      end
+    end
   end
 
-  it "is valid like instance" do
-    expect(@like).to be_valid
-  end
+  describe "check association" do
+    context "when is association for users" do
+      it "is one like for many users" do
+        expect(described_class.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
 
-  it "is invalid" do
-    other_like = build(:like, user_id: @user.id, picture_id: @picture.id)
-    other_like.valid?
-
-    expect(other_like).not_to be_valid
-  end
-
-  it "is reflected on assciation for users" do
-    expect(described_class.reflect_on_association(:user).macro).to eq :belongs_to
-  end
-
-  it "is reflected on assciation for pictures" do
-    expect(described_class.reflect_on_association(:picture).macro).to eq :belongs_to
+    context "when is association for pictures" do
+      it "is one like for many pictures" do
+        expect(described_class.reflect_on_association(:picture).macro).to eq :belongs_to
+      end
+    end
   end
 end
