77199e6d22748c33abadf87f5d1447c54f424dbb
ooooose <yuuki.oose1990@gmail.com>
2023-07-29 13:55:44 +0900

    [Add]Likeコントローラの作成

---
+++ api/app/controllers/api/v1/likes_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::LikesController < BaseController
+  before_action :set_picture, only: %i[create destroy]
+
+  def create
+    current_user.like(picture)
+    render json: { status: :ok, message: "you like successfully picture." }
+  end
+
+  def destroy
+    current_user.unlike(picture)
+    render json: { status: :ok, message: "you unlike successfully picture." }
+  end
+
+  private
+    def set_picture
+      picture = Picture.find(params[:picture_id])
+    end
+end
