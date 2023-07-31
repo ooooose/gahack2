ca558226bd18de6dd12e476b03959529eb32014c
ooooose <yuuki.oose1990@gmail.com>
2023-07-31 20:57:07 +0900

    [Add]ダミーComment追加

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -4,6 +4,10 @@ theme_3 = Theme.find_or_create_by(title: "怒ってるモチ")
 
 user = User.find_or_create_by(uid: 'aaa', name: 'oose', twitter_name: 'oose', avatar: 'sample_avatar', description: '')
 
-Picture.find_or_create_by(user_id: user.id, theme_id: theme_1.id, image: 'image_a', frame_id: 0)
-Picture.find_or_create_by(user_id: user.id, theme_id: theme_2.id, image: 'image_b', frame_id: 0)
-Picture.find_or_create_by(user_id: user.id, theme_id: theme_3.id, image: 'image_c', frame_id: 0)
+picture_1 = Picture.find_or_create_by(user_id: user.id, theme_id: theme_1.id, image: 'image_a', frame_id: 0)
+picture_2 = Picture.find_or_create_by(user_id: user.id, theme_id: theme_2.id, image: 'image_b', frame_id: 0)
+picture_3 = Picture.find_or_create_by(user_id: user.id, theme_id: theme_3.id, image: 'image_c', frame_id: 0)
+
+Comment.find_or_create_by(user_id: user.id, picture_id: picture_1.id, body: "コメントサンプル1")
+Comment.find_or_create_by(user_id: user.id, picture_id: picture_2.id, body: "コメントサンプル2")
+Comment.find_or_create_by(user_id: user.id, picture_id: picture_3.id, body: "コメントサンプル3")
