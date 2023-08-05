f4f936ae2250d5b98cb2dfa676ebc4890cab53f5
ooooose <yuuki.oose1990@gmail.com>
2023-08-05 10:13:53 +0900

    pictureAPIのテスト

--- api/spec/requests/api/v1/pictures_spec.rb
+++ api/spec/requests/api/v1/pictures_spec.rb
@@ -2,6 +2,28 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::Pictures", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "display all pictures" do
+      create_list(:picture, 10)
+      get "/api/v1/pictures"
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(json.length).to eq(10)
+    end
+  end
+
+  describe "POST /create" do
+    xit "successfully create picture" do
+      # 認証機能が作成できたら記述する。
+    end
+  end
+
+  describe "GET /show" do
+    it "display the specific picture" do
+      picture = create(:picture)
+      get "/api/v1/pictures/#{picture.id}"
+      json = JSON.parse(response.body)
+      expect(response).to have_http_status(:success)
+      expect(json['image']).to eq(picture.image)
+    end
   end
 end
