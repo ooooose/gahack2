7b7b7cdc31fc3f82f0cd2a48addbf55a0e7649a2
ooooose <yuuki.oose1990@gmail.com>
2023-08-05 10:06:33 +0900

    [Add]テーマAPIのテスト

--- api/spec/requests/api/v1/themes_spec.rb
+++ api/spec/requests/api/v1/themes_spec.rb
@@ -2,6 +2,24 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::Themes", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'display all themes' do
+      create_list(:theme, 10)
+      get "/api/v1/themes"
+      json = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(json.length).to eq(10)
+    end
+  end
+
+  describe "Get /show" do
+    it 'fetch the specific theme' do
+      theme = create(:theme)
+      get "/api/v1/themes/#{theme.id}"
+      json = JSON.parse(response.body)
+
+      expect(response.status).to eq(200)
+
+      expect(json['title']).to eq(theme.title)
+    end
   end
 end
