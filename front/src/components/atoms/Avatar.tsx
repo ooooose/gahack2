d530f816dc6947b41b288b61f7c66eb113c6cbf5
ooooose <yuuki.oose1990@gmail.com>
2023-08-05 10:51:56 +0900

    make fix

--- front/src/components/atoms/Avatar.tsx
+++ front/src/components/atoms/Avatar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Avatar as ChakraAvatar } from '@chakra-ui/react';
+import { Avatar as ChakraAvatar } from '@chakra-ui/react';
 
 // UserのAvatarをpropsで受け取る。Auth認証していたら使用のAvatarを使ってもらうよう条件分岐する。
 // Memo化する！
--- front/src/components/atoms/Logo.tsx
+++ front/src/components/atoms/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
-import { Image, Heading, Box } from '@chakra-ui/react';
+import { Heading, Box } from '@chakra-ui/react';
 
 const Logo = () => {
   return (
