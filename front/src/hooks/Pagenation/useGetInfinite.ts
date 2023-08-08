c4101ebd8fa773137af2cdd6f55281c0413957b7
ooooose <yuuki.oose1990@gmail.com>
2023-08-09 07:07:02 +0900

    SWRを用いた無限スクロールの実装

---
+++ front/src/hooks/Pagenation/useGetInfinite.ts
@@ -0,0 +1,7 @@
+import { useCallback } from "react"
+
+export const useGetInfinite = <Data = any>(
+  url: string, 
+) => {
+  // TODO 無限スクロールを実装
+}
--- front/src/providers/app.tsx
+++ front/src/providers/app.tsx
@@ -8,6 +8,7 @@ type AppProviderProps = {
   children: ReactNode;
 };
 
+
 const AppProvider = ({ children }: AppProviderProps) => {
   return (
     <>
