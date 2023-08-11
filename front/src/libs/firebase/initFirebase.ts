b5d69d99869d21d41a1a07f6c131a26f7092fb6e
ooooose <yuuki.oose1990@gmail.com>
2023-08-11 23:56:48 +0900

    Auth認証の準備

---
+++ front/src/libs/firebase/initFirebase.ts
@@ -0,0 +1,28 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PUBLIC_FIREBASE_MESSEGING_SENDER_ID,
+  appId: process.env.REACT_APP_PUBLIC_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_PUBLIC_FIREBASE_DATABASE_URL,
+};
+
+// Initialize Firebase
+// If app already exists, get that one
+const getFirebaseApp = () => {
+  if (getApps().length === 0) {
+    return initializeApp(firebaseConfig);
+  } else {
+    return getApp();
+  }
+};
+
+const app = getFirebaseApp();
+
+// Returns the Auth instance associated with the provided FirebaseApp.
+export const auth = getAuth(app);
