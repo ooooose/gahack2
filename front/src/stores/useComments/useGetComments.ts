fcecb321a3c2f39b86cfcd110d343ca5288ada3c
ooooose <yuuki.oose1990@gmail.com>
2023-09-19 06:25:22 +0900

    Add: Comment取得のフックを作成

---
+++ front/src/stores/useComments/useGetComments.ts
@@ -0,0 +1,13 @@
+import useSWR, { SWRResponse } from 'swr';
+import { Comment } from '../../types/comments';
+import { apiClient } from '../../utils/api-client';
+
+type Props = {
+  pictureId: number;
+};
+
+export const useGetComments = ({ pictureId }: Props): SWRResponse<Comment[]> => {
+  return useSWR(`/pictures/${pictureId}/comments`, (endpoint) =>
+    apiClient.apiGet<Comment[]>(endpoint).then((result) => result.data),
+  );
+}
