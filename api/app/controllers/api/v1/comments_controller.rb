7c3f20e97a19c85010adb8b56756daa97b50429f
ooooose <yuuki.oose1990@gmail.com>
2023-09-20 07:05:56 +0900

    fix: N+1対策

--- api/app/controllers/api/v1/comments_controller.rb
+++ api/app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CommentsController < BaseController
   def index
-    picture = Picture.find(params[:picture_id])
+    picture = Picture.includes(:user).find(params[:picture_id])
     @comments = picture.comments
 
     render json: @comments, each_serializer: Api::V1::CommentSerializer
