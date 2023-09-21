7c05a2c7fbccbc9ca1d2ca0b6f80fc0ad77d8d18
ooooose <yuuki.oose1990@gmail.com>
2023-09-22 06:18:55 +0900

    rubocop -a

--- api/spec/requests/api/v1/likes_spec.rb
+++ api/spec/requests/api/v1/likes_spec.rb
@@ -2,15 +2,15 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::Likes", type: :request do
   describe "GET /pictures/{picture_id}/likes" do
-    xit 'is able to get all likes of the picture' do
-      @picture = create(:picture)
-      create_list(:like, 10, picture_id: @picture.id)
-      get "/api/v1/pictures/#{@picture.id}/likes"
+    xit "all likes for picture" do
+      picture = create(:picture)
+      create_list(:like, 10, picture_id: picture.id)
+      get "/api/v1/pictures/#{picture.id}/likes"
       json = JSON.parse(response.body)
 
-      expect(response.status).to eq(200)
+      # expect(response.status).to eq(200)
 
-      expect(json['data'].length).to eq(10)
+      expect(json["data"].length).to eq(10)
     end
   end
 end
--- api/spec/support/firebase_auth_helper.rb
+++ api/spec/support/firebase_auth_helper.rb
@@ -1,34 +1,33 @@
-include FirebaseAuth
-
 module FirebaseAuthHelper
+  include FirebaseAuth
   def sign_in_with_google
     # テスト用のFirebaseユーザーを作成し、Googleプロバイダーでログイン
     @firebase_user = create_firebase_user_with_google
-    
+
     # 認証ユーザーとして返す。
-    return @firebase_user
+    @firebase_user
   end
 
   private
 
-  def create_firebase_user_with_google
-    # Firebase Authenticationのモックを使用してテスト用のユーザーを作成
-    user = Firebase::Auth::User.new(
-      uid: 'test_user_uid',
-      display_name: 'Test User',
-      email: 'test@example.com',
-      provider: 'google.com'
-    )
-    Firebase::Auth::User.create(user)
+    def create_firebase_user_with_google
+      # Firebase Authenticationのモックを使用してテスト用のユーザーを作成
+      user = Firebase::Auth::User.new(
+        uid: "test_user_uid",
+        display_name: "Test User",
+        email: "test@example.com",
+        provider: "google.com"
+      )
+      Firebase::Auth::User.create!(user)
 
-    # テスト用のユーザーをGoogleプロバイダーでログイン
-    Firebase::Auth::AuthHash.new(
-      provider: 'google.com',
-      uid: user.uid,
-      info: {
-        email: user.email,
-        name: user.display_name
-      }
-    )
-  end
+      # テスト用のユーザーをGoogleプロバイダーでログイン
+      Firebase::Auth::AuthHash.new(
+        provider: "google.com",
+        uid: user.uid,
+        info: {
+          email: user.email,
+          name: user.display_name,
+        }
+      )
+    end
 end
