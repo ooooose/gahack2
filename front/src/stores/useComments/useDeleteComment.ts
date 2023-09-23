29311aa35fd76d58b7cb358348a1c94754c4aa0a
ooooose <yuuki.oose1990@gmail.com>
2023-09-23 09:29:38 +0900

    add: コメント削除のフックを作成

---
+++ front/src/stores/useComments/useDeleteComment.ts
@@ -0,0 +1,19 @@
+import useSWRMutation, { SWRMutationResponse } from 'swr/mutation';
+import { apiClient } from '../../utils/api-client';
+import { Comment } from '../../types/comments';
+
+type Props = {
+  pictureId: number;
+  comment: Comment;
+  params: {
+    picture_id: number;
+  };
+};
+
+export const useDeleteComment = async ({
+  pictureId,
+  comment,
+  params,
+}: Props) => {
+  return await apiClient.apiDelete(`/pictures/${pictureId}/comments/${comment.id}`, params).then((response) => response)
+};
