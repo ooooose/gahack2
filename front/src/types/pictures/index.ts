04ae9d2442861fa581dc7b1d4c128b3a204a9bc3
ooooose <yuuki.oose1990@gmail.com>
2023-09-24 21:33:12 +0900

    Add: 型にプロパティを追加

--- front/src/types/pictures/index.ts
+++ front/src/types/pictures/index.ts
@@ -1,12 +1,16 @@
 import { Comment } from '../comments';
 import { Like } from '../likes';
+import { Theme } from '../themes';
+import { User } from '../users';
 
 export type Picture = {
   id: number;
   image: string;
   frameId: number;
-  createdAt: string;
+  created_at: string;
   comments?: Comment[];
+  theme: Theme
+  user: User
   likes?: Like[];
 };
 
