024466cb3ad590ad1bec734a9ee548cebcf92983
ooooose <yuuki.oose1990@gmail.com>
2023-09-25 07:20:43 +0900

    Add: ページの作成

---
+++ front/src/components/pages/LoginPage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  Flex,
+  Stack,
+  Heading,
+} from '@chakra-ui/react'
+import LoginForm from "../molecules/LoginForm";
+
+function LoginPage() {
+  return (
+    <Flex
+      align={'center'}
+      justify={'center'}>
+      <Stack spacing={4} mx={'auto'} w={'md'} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>ログイン</Heading>
+        </Stack>
+        <LoginForm />
+      </Stack>
+    </Flex>
+  )
+}
+
+export default LoginPage;
