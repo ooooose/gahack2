af4c1151070390ca7a2d698a1bd06ed56e08af6c
ooooose <yuuki.oose1990@gmail.com>
2023-09-25 20:08:42 +0900

    fix: 空文字の入力を拒否

--- front/src/components/atoms/CommentModal.tsx
+++ front/src/components/atoms/CommentModal.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
 import {
   Modal,
   ModalOverlay,
@@ -11,6 +14,7 @@ import {
   useDisclosure,
   Box,
   Text,
+  FormControl,
   Tooltip,
   Input,
   Button,
@@ -27,33 +31,50 @@ type Props = {
 
 const CommentModal = ({ comments, pictureId }: Props) => {
   const [comment, setComment] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
   const params = {
     picture_id: pictureId,
     body: comment,
   };
+
   const { trigger, isMutating } = usePostComment({ pictureId, params });
-  const submitComment = () => {
+
+  const onSubmit = handleSubmit((data) => {
+    if (data.body.trim() === '') {
+      return;
+    }
+    trigger();
+    setComment('');
+    reset();
+  });
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.nativeEvent.isComposing || e.key !== 'Enter' || comment.length === 0 || comment.trim() === '')
+      return;
     trigger();
     setComment('');
   };
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.nativeEvent.isComposing || e.key !== 'Enter' || comment.length === 0)
-      return;
-    submitComment();
-  };
 
   const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
       <Tooltip label="コメント" bg="gray.400" fontSize="11px">
-        <Box display={'flex'} gap={1}>
+        <Box display="flex" gap={1}>
           <Icon
             as={BiComment}
             fontSize={20}
             onClick={onOpen}
-            cursor={'pointer'}
+            cursor="pointer"
           />
-          <Text pointerEvents={'none'} fontSize={12}>
+          <Text pointerEvents="none" fontSize={12}>
             {comments.length}
           </Text>
         </Box>
@@ -64,7 +85,7 @@ const CommentModal = ({ comments, pictureId }: Props) => {
           <ModalHeader>コメント</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Box w={'100%'} h={'50vh'} overflowX={'auto'}>
+            <Box w="100%" h="50vh" overflowX="auto">
               {comments.map((comment) => (
                 <Comment
                   key={comment.id}
@@ -75,29 +96,36 @@ const CommentModal = ({ comments, pictureId }: Props) => {
             </Box>
           </ModalBody>
           <ModalFooter>
-            {/* コメントフォームとSubmitボタンを設置予定 */}
-            <Box width={'100%'} mx={'auto'}>
-              <Input
-                mb={2}
-                type="text"
-                placeholder="コメントを入力"
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                onKeyDown={handleKeyDown}
-              />
-              <Button
-                float={'right'}
-                colorScheme="blue"
-                type="submit"
-                variant="solid"
-                onClick={() => {
-                  submitComment();
-                }}
-                isDisabled={comment === '' ? true : false}
-                isLoading={isMutating}
-              >
-                投稿する
-              </Button>
+            <Box width="100%" mx="auto">
+              <form onSubmit={onSubmit}>
+                <FormControl isInvalid={!!errors.body}>
+                  <Input
+                    id="body"
+                    mb={2}
+                    type="text"
+                    {...register('body')}
+                    placeholder="コメントを入力"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                  />
+                </FormControl>
+                {errors.body && (
+                  <Text color="red.500" fontSize="sm">
+                    {String(errors.body.message)}
+                  </Text>
+                )}
+                <Button
+                  float="right"
+                  colorScheme="blue"
+                  type="submit"
+                  variant="solid"
+                  isDisabled={!comment || isMutating}
+                  isLoading={isMutating}
+                >
+                  投稿する
+                </Button>
+              </form>
             </Box>
           </ModalFooter>
         </ModalContent>
@@ -105,5 +133,9 @@ const CommentModal = ({ comments, pictureId }: Props) => {
     </>
   );
 };
+
+const schema = z.object({
+  body: z.string().nonempty({ message: '空文字のみの入力は無効です' }),
+});
 
 export default CommentModal;
